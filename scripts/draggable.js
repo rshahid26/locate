d0d82a23ad8c14a9599cbbc8bd1c5b09ab913020
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2022-10-24 03:22:29 -0400

    draggable update

--- scripts/draggable.js
+++ scripts/draggable.js
@@ -1,10 +1,30 @@
 //run drag on mousedown and mouse movement events
+/*
 document.getElementById("window_outer")
-    .addEventListener("mousemove", drag());
+    .addEventListener("mousemove", e => {
+        drag(e);
+    });
 document.getElementById("window_outer")
-    .addEventListener("mousedown", drag());
+    .addEventListener("mousedown", e => {
+
+    });
+
+ */
 
 
-function drag() {
-    return console.log("moving!");
+function draggable(div) {
+    //save position variables on mousedown
+    div.addEventListener("mousedown", e => {
+
+        let x = div.getBoundingClientRect().x;
+        let y = div.getBoundingClientRect().y;
+
+        console.log("(" + x + "," + y + ")");
+
+        div.onmousemove = () => {
+
+        }
+    })
 }
+
+draggable(document.getElementById("window_outer"));
