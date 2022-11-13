8b849067afd6bb0aa0e17a1b309833975786c0a5
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2022-11-13 05:50:01 -0500

    removed tab key

--- scripts/draggable.js
+++ scripts/draggable.js
@@ -1,78 +1,62 @@
 "use strict";
 
-//make window_outer elements draggable after pressing tab
-let tabPressed = false;
-
-document.addEventListener("keydown", (e) => {
-        if (e.code === "Tab") {
-
-            tabPressed = true;
-
-            for (let element of document.getElementsByClassName("window_outer"))
-                dragElement(element);
-        }
-    });
-
+dragElement(document.getElementsByClassName("window_outer")[0]);
 
 export function dragElement(div) {
-    if (tabPressed) {
+    //declare variables for mousedown and mousemove positions
+    let x1 = 0, y1 = 0,
+        x2 = 0, y2 = 0;
 
-        //declare variables for mousedown and mousemove positions
-        let x1 = 0, y1 = 0,
-            x2 = 0, y2 = 0;
+    div.onmousedown = dragMouse;
 
-        div.onmousedown = dragMouse;
+    function dragMouse(e) {
+        e.preventDefault();
 
-        function dragMouse(e) {
-            e.preventDefault();
+        //get the mouse cursor position at startup
+        x1 = e.clientX;
+        y1 = e.clientY;
 
-            //get the mouse cursor position at startup
-            x1 = e.clientX;
-            y1 = e.clientY;
+        //ensure mouse position is not in a ticker box
+        for (let ticker of document.getElementsByClassName("ticker")) {
 
-            //ensure mouse position is not in a ticker box
-            for (let ticker of document.getElementsByClassName("ticker")) {
+            //assign distance relative to ticker coordinates if appropriate
+            let xDead = x1 > ticker.getBoundingClientRect().x ?
+                x1 - ticker.getBoundingClientRect().x : undefined;
 
-                //assign distance relative to ticker coordinates if appropriate
-                let xDead = x1 > ticker.getBoundingClientRect().x ?
-                    x1 - ticker.getBoundingClientRect().x : undefined;
+            let yDead = y1 > ticker.getBoundingClientRect().y ?
+                y1 - ticker.getBoundingClientRect().y : undefined;
 
-                let yDead = y1 > ticker.getBoundingClientRect().y ?
-                    y1 - ticker.getBoundingClientRect().y : undefined;
+            //compare distances with width/height of any ticker box (all identical)
+            if (xDead < ticker.getBoundingClientRect().width &&
+                yDead < ticker.getBoundingClientRect().height) {
 
-                //compare distances with width/height of any ticker box (all identical)
-                if (xDead < ticker.getBoundingClientRect().width &&
-                    yDead < ticker.getBoundingClientRect().height) {
+                ticker.focus();
+                return -1;
 
-                    ticker.focus();
-                    return -1;
-
-                }
             }
-
-            //call functions based on cursor movement
-            document.onmouseup = endDraggable;
-            document.onmousemove = elementDrag;
         }
 
-        function elementDrag(e) {
-            e.preventDefault();
+        //call functions based on cursor movement
+        document.onmouseup = endDraggable;
+        document.onmousemove = elementDrag;
+    }
 
-            //obtain new coordinates and their distances from the original
-            x2 = x1 - e.clientX;
-            y2 = y1 - e.clientY;
-            x1 = e.clientX;
-            y1 = e.clientY;
+    function elementDrag(e) {
+        e.preventDefault();
 
-            //set the element's new position accordingly
-            div.style.top = (div.offsetTop - y2) + "px";
-            div.style.left = (div.offsetLeft - x2) + "px";
-        }
+        //obtain new coordinates and their distances from the original
+        x2 = x1 - e.clientX;
+        y2 = y1 - e.clientY;
+        x1 = e.clientX;
+        y1 = e.clientY;
 
-        function endDraggable() {
-            document.onmouseup = null;
-            document.onmousemove = null;
-        }
+        //set the element's new position accordingly
+        div.style.top = (div.offsetTop - y2) + "px";
+        div.style.left = (div.offsetLeft - x2) + "px";
+    }
 
+    function endDraggable() {
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
 }
