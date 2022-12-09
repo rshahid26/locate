581a02e18805227e8a3148ba144e228cfea96a00
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2022-12-08 21:51:02 -0500

    port

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const http = require('http');
+const path = require('path');
+
+// Development server
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-
+    console.log(req.method, res.url);
     res.end('connected!');
-
 });
 
-server.listen(8080, 'localhost', () => {
-    console.log('server is running!');
-});
 
+
+server.listen(PORT, 'localhost', () => {
+
+    console.log('server is running! on ${PORT}');
+
+});
