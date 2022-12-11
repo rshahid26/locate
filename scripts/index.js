9f2d20085893d6cf2def4aa9795bc75746141637
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2022-12-11 05:29:50 -0500

    helper function

--- scripts/index.js
+++ scripts/index.js
@@ -25,16 +25,18 @@ for (let element of forms)
             .catch(error => {
                 console.log(error);
             });
-
-        // Auto-select input fields on submit
-        element.getElementsByClassName("ticker")[0].select();
     })
 
+// Auto-select input fields on submit
+for (let element of forms)
+    element.addEventListener("submit", (e) => {
+
+        element.getElementsByClassName("ticker")[0].select();
+    })
 // Auto-select input fields on click
 const tickers = document.getElementsByClassName("ticker");
 for (let element of tickers)
     element.addEventListener("click", (e) => {
 
         element.select();
-
     })
