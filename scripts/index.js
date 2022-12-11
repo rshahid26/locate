12b1dc60bcd719eb6a9edbf2601d0b25a4033085
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2022-12-11 04:04:37 -0500

    ticker selection fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
 "use strict";
+
+// Retrieve locate data through http requests
+const forms = document.getElementsByClassName("ticker_form");
+for (let element of forms)
+    element.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+
+        const ticker = element.getElementsByClassName("ticker")[0]
+            .value.toUpperCase();
+        const time = "DAY";
+
+
+
+        console.log(ticker + " " + time);
+
+        // Auto-select input fields on submit
+        element.getElementsByClassName("ticker")[0].select();
+    })
+
+// Auto-select input fields on click
+const tickers = document.getElementsByClassName("ticker");
+for (let element of tickers)
+    element.addEventListener("click", (e) => {
+
+        element.select();
+
+    })
