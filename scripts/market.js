7604a942f5b341dff0aadcb547e6d9763775c56e
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2022-12-25 09:36:27 -0500

    random data

--- scripts/market.js
+++ scripts/market.js
@@ -34,6 +34,61 @@ function dummy(canvas) {
         .catch(reject => console.log(reject));
 }
 
+var barCount = 60;
+var initialDateStr = '01 Apr 2017 00:00 Z';
+
+var canvas = document.getElementById('chart1').getContext('2d');
+
+let barData2 = {
+
+}
+var barData = getRandomData(initialDateStr, barCount);
+
+var chart = new Chart(canvas, {
+    type: 'candlestick',
+    data: {
+        datasets: [{
+            label: 'HKD',
+            data: barData
+        }]
+    }
+});
+
+var getRandomInt = function(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+};
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function randomBar(date, lastClose) {
+    var open = +randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
+    var close = +randomNumber(open * 0.95, open * 1.05).toFixed(2);
+    var high = +randomNumber(Math.max(open, close), Math.max(open, close) * 1.1).toFixed(2);
+    var low = +randomNumber(Math.min(open, close) * 0.9, Math.min(open, close)).toFixed(2);
+    return {
+        x: date.valueOf(),
+        o: open,
+        h: high,
+        l: low,
+        c: close
+    };
+
+}
+
+function getRandomData(dateStr, count) {
+    var date = luxon.DateTime.fromRFC2822(dateStr);
+    var data = [randomBar(date, 30)];
+    while (data.length < count) {
+        date = date.plus({days: 1});
+        if (date.weekday <= 5) {
+            data.push(randomBar(date, data[data.length - 1].c));
+        }
+    }
+    return data;
+}
+
 function createChart(canvas, data) {
     let xValues = [];
     let yValues = [];
