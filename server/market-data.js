d4114129298f1692420cde2c460562a4984c3f80
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2022-12-28 23:41:36 -0500

    market-data url

--- server/market-data.js
+++ server/market-data.js
@@ -6,7 +6,7 @@ function loadData(symbol) {
     return new Promise((resolve, reject) => {
 
         const key = "CKEJIMJVB8FKOX6D";
-        const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=daily&slice=year1month1&apikey=${key}`;
+        const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=daily&apikey=${key}`;
 
         request.get({
             url: url,
