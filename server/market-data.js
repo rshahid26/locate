b93d269a2fad2fd63d80adee56cdfd324f695edd
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2023-01-15 12:01:14 -0500

    titles respond now

--- server/market-data.js
+++ server/market-data.js
@@ -5,7 +5,7 @@ module.exports = {loadData};
 function loadData(symbol) {
     return new Promise((resolve, reject) => {
 
-        const key = "CKEJIMJVB8FKOX6D";
+        const key = 'CKEJIMJVB8FKOX6D';
         const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=${symbol}&interval=30min&apikey=${key}`;
 
         request.get({
@@ -14,7 +14,7 @@ function loadData(symbol) {
             headers: {'User-Agent': 'request'}
         }, (err, res, data) => {
             if (err || (res.statusCode !== 200)) {
-                console.log('Status:', res.statusCode + ' Error:', err);
+                console.log('Status:', res.statusCode + ' Data Error:', err);
                 reject(err);
             } else {
                 resolve(data);
@@ -22,4 +22,41 @@ function loadData(symbol) {
         });
 
     });
+}
+
+/*
+Find symbol title by scraping yahoo finance
+ */
+function loadTitle(symbol) {
+    return new Promise((resolve, reject) => {
+
+        request.get({
+            url: `https://www.finance.yahoo.com/quote/${symbol}?p=${symbol}&.tsrc=fin-srch`,
+            headers: {
+                'User-Agent': 'request',
+                'Content-Type': 'text/html'
+            }
+        }, (err, res, body) => {
+            if (err || (res.statusCode !== 200)) {
+                console.log('Status:', res.statusCode + ' Title Error:', err);
+                reject(err);
+            } else {
+                resolve(parseTitle(body, symbol));
+            }
+        });
+
+    });
+}
+
+function parseTitle(body, symbol) {
+
+    let origin = body.toString().indexOf(`(${symbol})`);
+    let tag = '>';
+
+    for (let i = 0; i < origin; i++) {
+        if (body.substring(origin - i, origin).indexOf(tag) > -1)
+            return body.substring(origin - i + tag.length, origin - 1);
+    }
+
+    return symbol + " Unknown";
 }
